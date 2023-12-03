5a46ed350d9daa210fc6001cb56eb9f9a4b42f77
aizadrzo <aizadridzoo@gmail.com>
2023-12-03 17:54:43 +0800

    remove card components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,5 @@ export { default as Text } from "./Text/Text";
 export { default as Accordion } from "./Accordion/Accordion";
 export { default as Badge } from "./Badge/Badge";
 export { default as Link } from "./Link/Link";
-export { default as Card } from "./Card/Card";
 export { default as Footer } from "./Footer/Footer";
 export { default as Layout } from "./Layout/Layout";
